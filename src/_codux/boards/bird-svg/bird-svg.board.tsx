b3f7a4cd685a2b2c555bfd35e2e9a721986460f7
Codux <codux@codux.com>
2024-07-04 11:03:05 +0530

    delted error files

--- src/_codux/boards/bird-svg/bird-svg.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { BirdSvg } from '../../../../bird-svg/bird-svg';
-
-export default createBoard({
-    name: 'Bird_svg',
-    Board: () => <BirdSvg />,
-    isSnippet: true,
-});
