2c84dd7a0d002da98ece56ab8c670dd83240ca8a
Codux <codux@codux.com>
2024-07-05 15:51:27 +0530

    it works ?

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -6,7 +6,7 @@ export default createBoard({
     Board: () => <App />,
     isSnippet: true,
     environmentProps: {
-        canvasHeight: 746, // Synchronize canvas height with window height
+        canvasHeight: 741, // Synchronize canvas height with window height
         canvasWidth: 1501, // Synchronize canvas width with window width
         windowWidth: window.innerWidth,
         windowHeight: window.innerHeight,
