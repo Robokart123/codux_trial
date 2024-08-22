367b3ec0d72d4f3d4420f59cff451a75c0b895cb
Codux <codux@codux.com>
2024-08-22 12:34:20 +0530

    Added Filler Text

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import classNames from 'classnames';
 import styles from './home-page.module.scss';
 import LogoSvg from '../../assets/Bharat_logo.svg?react'; // Correct usage
+import GapSvg from '../../assets/GapBetweenEducation.svg?react'; // Correct usage
 import videoSrc from '../../assets/Demo_Video.mp4'; // Adjust the path as needed
 
 export default interface HomePageProps {
@@ -43,7 +44,7 @@ export const HomePage = ({ className }: HomePageProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <div className={styles.logo}>
-                    <LogoSvg className={styles.logo} />
+                <LogoSvg className={styles.logo} />
             </div>
             <div className={styles.paragraph}>
                 <div className={styles.text}>Innovate. Inspire. Ignite.</div>
@@ -59,6 +60,32 @@ export const HomePage = ({ className }: HomePageProps) => {
                     loop
                 ></video>
                 <div className={styles.gradient_BotToTop}></div>
+            </div>
+            <div className={styles.layout}>
+                <div className={styles.secondary_title}>
+                    Bridging the Gap Between Students &amp; Careers
+                </div>
+                <div className={styles.secondary_paragraph}>
+                    <ul className={styles.bullet_points}>
+                        <li>
+                            There is a noticeable lack of skill-based education in schools, leading
+                            to a gap between academic knowledge and practical skills needed in the
+                            real world.
+                        </li>
+                        <li>
+                            We  offer students the opportunity to learn modern life skills by
+                            providing them with instruments and training on contemporary equipment.
+                        </li>
+                        <li>
+                            The majority of India&apos;s population lives in rural areas. We provide
+                            our technological solutions to these schools to support the sustainable
+                            development of the country.
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.image}>
+                    <GapSvg className={styles.image} />
+                </div>
             </div>
         </div>
     );
